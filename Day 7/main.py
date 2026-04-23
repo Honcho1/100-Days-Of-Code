35f1032021fa6292505763346ad4ccff2241d20e
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2026-04-23 10:31:04 +0100

    Write the logic for the hangman

--- Day 7/main.py
+++ Day 7/main.py
@@ -2,3 +2,46 @@
 import random
 from hangman_words import word_list
 from hangman_art import logo, stages
+
+print(logo)
+
+chosen_word = random.choice(word_list)
+
+lives = 6
+
+display = []
+guessed_letters = []
+
+for letter in chosen_word:
+    display.append("_")
+
+game_over = False
+
+while not game_over:
+    print(stages[lives])
+    print(" ".join(display))
+
+    guess = input("Guess a letter: ").lower()
+
+    correct_guess = False
+
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+
+        if letter == guess:
+            display[position] = guess
+            correct_guess = True
+
+    if not correct_guess:
+        lives -= 1
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+
+        if lives == 0:
+            game_over = True
+            print(stages[lives])
+            print("************************** YOU LOSE! **************************")
+
+    if "_" not in display:
+        game_over = True
+        print(stages[lives])
+        print("************************** YOU WIN! **************************")
