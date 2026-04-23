d801bcfa51d9ac586d98977382c7ebe8195e334b
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2026-04-23 14:48:08 +0100

    Finish task 2

---
+++ Day 8/task2.py
@@ -0,0 +1,6 @@
+def greet_with_name(name):
+    print(f"Hello, {name}!")
+    print(f"Welcome to Python programming, {name}.")
+    print(f"Let's learn together, {name}!")
+
+greet_with_name("Daniel")
