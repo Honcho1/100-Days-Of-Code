e7a78d7a7251e49aaac88331eb671bbb56752649
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2026-04-24 23:45:52 +0100

    Add the list of the student scores for task 1

---
+++ Day 9/task1.py
@@ -0,0 +1,12 @@
+student_scores = {
+    "Alice": 85,
+    "Ben": 92,
+    "Charlie": 78,
+    "Diana": 95,
+    "Ethan": 88,
+    "Fiona": 76,
+    "George": 90,
+    "Hannah": 82,
+    "Ian": 79,
+    "Julia": 94,
+}
