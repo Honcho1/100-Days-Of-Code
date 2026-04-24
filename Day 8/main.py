fbdf885e657f05b824dcf2a10a13ba376eb57008
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2026-04-24 04:51:12 +0100

    Update the code to restart

--- Day 8/main.py
+++ Day 8/main.py
@@ -7,9 +7,6 @@ print(logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 def caesar(original_text, shift_amount, cipher_direction):
     result_text = ""
@@ -29,4 +26,25 @@ def caesar(original_text, shift_amount, cipher_direction):
 
     print(f"The {cipher_direction}d text is {result_text}")
 
-caesar(original_text=text, shift_amount=shift, cipher_direction=direction)
+should_continue = True
+
+while should_continue:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+
+    caesar(
+        original_text=text,
+        shift_amount=shift,
+        cipher_direction=direction
+    )
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no':\n").lower()
+
+    if restart == "no":
+        should_continue = False
+        print("Goodbye!")
+
