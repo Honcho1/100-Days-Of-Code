1ecd491a444df5d97381cf40249738204d7a3fec
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2026-04-24 04:35:20 +0100

    Add the code to print the logo

--- Day 8/main.py
+++ Day 8/main.py
@@ -1,5 +1,7 @@
 # Caesar Cipher Program
 from unittest import result
+from art import logo
+print(logo)
 
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
