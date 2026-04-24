fd1004a287552d5a107dd702e1706d5a3764077b
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2026-04-24 23:51:40 +0100

    Finish task 1

--- Day 9/task1.py
+++ Day 9/task1.py
@@ -10,3 +10,19 @@ student_scores = {
     "Ian": 79,
     "Julia": 94,
 }
+
+student_grades = {}
+
+for student in student_scores:
+    score = student_scores[student]
+
+    if score >= 91:
+        student_grades[student] = "Outstanding"
+    elif score >= 81:
+        student_grades[student] = "Exceeds Expectations"
+    elif score >= 71:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
+
+print(student_grades)
