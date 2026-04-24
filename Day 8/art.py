b49ba8d8f8df2bfbdff62d3472c21ae6e32265ad
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2026-04-24 04:34:15 +0100

    Add the module for the logo

---
+++ Day 8/art.py
@@ -0,0 +1,17 @@
+logo = r'''
+           
+ ,adPPYba,  ,adPPYYba,  ,adPPYba,  ,adPPYba,  ,adPPYba,  8b,dPPYba,  
+a8"     ""  ""     `Y8  I8[    ""  I8[    ""  I8[    ""  88P'   "Y8  
+8b          ,adPPPPP88   `"Y8ba,    `"Y8ba,   `"Y8ba,   88          
+"8a,   ,aa  88,    ,88  aa    ]8I  aa    ]8I  aa    ]8I  88          
+ `"Ybbd8"'  `"8bbdP"Y8  `"YbbdP"'  `"YbbdP"'  `"YbbdP"'  88          
+                                                                     
+ ,adPPYba,  88  88888888ba,  88        88  88888888888  88888888ba   
+a8"     "8a 88  88      "8b  88        88  88           88      "8b  
+8b       d8 88  88      ,8P  88        88  88           88      ,8P  
+"8a,   ,a8" 88  88aaaad8P"   Y8,    ,8P  88           88aaaad8P"   
+ `"YbbdP"'  88  88""""88"     `"YbbdP"'   88           88""""88"    
+             88  88    `8b                 88           88    `8b    
+             88  88     `8b                88           88     `8b   
+             88  88      `8b               88           88      `8b  
+'''
