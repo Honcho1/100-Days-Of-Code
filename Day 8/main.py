5cb4abfe3b90e3c385b1613a435e38af889c3e4d
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2026-04-24 04:27:36 +0100

    Update the code to add the decode function

--- Day 8/main.py
+++ Day 8/main.py
@@ -1,19 +1,26 @@
 # Caesar Cipher Program
+from unittest import result
+
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(original_text, shift_amount):
-    cipher_text = ""
+def caesar(original_text, shift_amount, cipher_direction):
+    result_text = ""
 
     for letter in original_text:
         position = alphabet.index(letter)
-        new_position = position + shift_amount
-        cipher_text += alphabet[new_position % len(alphabet)]
 
-    print(f"The encoded text is {cipher_text}")
+        if cipher_direction == "encode":
+            new_position = position + shift_amount
+        elif cipher_direction == "decode":
+            new_position = position - shift_amount
 
-if direction == "encode":
-    encrypt(text, shift)
+        result_text += alphabet[new_position % 26]
+
+    print(f"The {cipher_direction}d text is {result_text}")
+
+caesar(original_text=text, shift_amount=shift, cipher_direction=direction)
