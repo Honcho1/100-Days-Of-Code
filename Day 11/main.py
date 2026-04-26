812593ed2eab3f966e5b229c29459f43b934e64f
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2026-04-26 08:22:08 +0100

    Write the function to play the game

--- Day 11/main.py
+++ Day 11/main.py
@@ -34,3 +34,38 @@ def compare(user_score, computer_score):
         return "You win 😃"
     else:
         return "You lose 😤"
+    
+def play_game():
+    user_cards = []
+    computer_cards = []
+    game_over = False
+
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
+
+    while not game_over:
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+
+        print(f"\nYour cards: {user_cards}, current score: {user_score}")
+        print(f"Computer's first card: {computer_cards[0]}")
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            game_over = True
+        else:
+            should_continue = input("Type 'y' to get another card, type 'n' to pass:")
+
+            if should_continue == 'y':
+                user_cards.append(deal_card())
+            else:
+                game_over = True
+
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_card())
+        computer_score = calculate_score(computer_cards)
+
+    print(f"\nYour final hand: {user_cards}, final score: {user_score}")
+    print(f"Computer's final hand: {computer_cards}, final score: {computer_score}")
+    print(compare(user_score, computer_score))
+    
