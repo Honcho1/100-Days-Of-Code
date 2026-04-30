6146ae316d5c5c8e35c8e01ae50abcd3b9745be7
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2026-04-30 03:53:23 +0100

    Write the function to play the game

--- Day 12/project.py
+++ Day 12/project.py
@@ -20,3 +20,31 @@ def set_difficulty():
         return 10
     else:
         return 5
+    
+def game():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+
+    number = random.randint(1, 100)
+    attempts = set_difficulty()
+
+    game_over = False
+
+    while not game_over:
+        print(f"You have {attempts} attempts remaining.")
+
+        guess = int(input("Make a guess: "))
+
+        if check_guess(guess, number):
+            game_over = True
+        else:
+            attempts -= 1
+
+            if attempts == 0:
+                print("You've run out of guesses,. You lose.")
+                print(f"The number was {number}.")
+                game_over = True
+            else:
+                print("Guess again.")
+
+game()
