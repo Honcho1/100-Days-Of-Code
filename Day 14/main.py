34e88b391cf71f91d665d0eb05533f12207c0ae3
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2026-05-02 04:24:13 +0100

    Write the function to pick a random account

--- Day 14/main.py
+++ Day 14/main.py
@@ -3,3 +3,9 @@ import random
 from art import logo, vs
 from game_data import data
 
+def get_random_account(used_account=None):
+    """Pick a random account, ensuring it's not the same as the used_account."""
+    choice = random.choice(data)
+    while choice == used_account:
+        choice = random.choice(data)
+    return choice
