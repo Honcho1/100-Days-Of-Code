b0163ff989dea1db6e7d2c7b73315f9526825e28
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2026-05-02 04:51:48 +0100

    Complete the logic of the higher lower game

--- Day 14/main.py
+++ Day 14/main.py
@@ -35,3 +35,14 @@ def play_game():
         print(vs)
         format_account(account_b, 'B')
         user_answer = input("\nWho has more followers? Type 'A' or 'B': ").lower()
+
+        if check_answer(user_answer, account_a, account_b):
+            score += 1
+            print("\n" * 20) # Clear the screen
+            print(f"You're right! Current score: {score}.")
+            account_a = account_b # The winner becomes the new account A
+        else:
+            game_over = True
+            print(f"\nSorry, that's wrong. Final score: {score}.")
+
+play_game()
