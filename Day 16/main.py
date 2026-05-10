65da144c3206a8d20fa7403beb8d4cfa6586ba31
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2026-05-10 07:42:54 +0100

    Write the logic for the coffee machine

--- Day 16/main.py
+++ Day 16/main.py
@@ -5,3 +5,23 @@ from coffee_maker import CoffeeMaker
 menu = Menu()
 coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
+
+machine_on = True
+
+while machine_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ").lower()
+
+    if choice == "off":
+        machine_on = False
+    
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+
+    else:
+        drink = menu.find_drink(choice)
+        if drink and coffee_maker.is_resource_sufficient(drink):
+            money_machine.process_coins()
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
