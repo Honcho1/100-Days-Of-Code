a13a93ef89822619d11d152df9379d2a43313702
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2026-05-19 23:31:55 +0100

    Create our new quiz

--- Day 17/main.py
+++ Day 17/main.py
@@ -3,3 +3,5 @@ from data import question_data
 from quiz_brain import QuizBrain
 
 question_bank = [Question(question["text"], question["answer"]) for question in question_data]
+
+quiz = QuizBrain(question_bank)
