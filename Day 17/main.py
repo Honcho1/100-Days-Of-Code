173fafe2bf6cbc35cb04c1adce7f10e14904de7d
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2026-05-19 23:30:17 +0100

    Import the QuizBrain class

--- Day 17/main.py
+++ Day 17/main.py
@@ -1,4 +1,5 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = [Question(question["text"], question["answer"]) for question in question_data]
