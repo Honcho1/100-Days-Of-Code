bfe066e96d0658d2b9735eeb4d6b835f9ecd1fee
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2026-05-20 00:29:05 +0100

    Add a new atrtribute to keep track of the user's score

--- Day 17/quiz_brain.py
+++ Day 17/quiz_brain.py
@@ -2,6 +2,7 @@ class QuizBrain:
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
+        self.score = 0
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
@@ -10,9 +11,15 @@ class QuizBrain:
     
     def check_answer(self, user_answer):
         current_question = self.question_list[self.question_number]
-        return user_answer.strip().title() == current_question.answer
+        is_correct = user_answer.strip().title() == current_question.answer
+        if is_correct:
+            self.score += 1
+            print("You got it right!")
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was: {current_question.answer}.")
+        print(f"Your current score is: {self.score}/{self.question_number + 1}\n")
+        return is_correct
     
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
-    
-    
