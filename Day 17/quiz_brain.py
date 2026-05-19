26f5b109565c6f790b36b7477ad1099ce73e89f7
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2026-05-20 00:24:20 +0100

    Create a method to check if the user's answer is correct or wrong

--- Day 17/quiz_brain.py
+++ Day 17/quiz_brain.py
@@ -8,6 +8,10 @@ class QuizBrain:
         user_answer = input(f"Q.{self.question_number + 1}: {current_question.text} (True/False): ")
         return user_answer
     
+    def check_answer(self, user_answer):
+        current_question = self.question_list[self.question_number]
+        return user_answer.strip().title() == current_question.answer
+    
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
     
