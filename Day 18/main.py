56d3e0c5337115dc456f0f240d5f6f5c78dc773b
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2026-05-26 22:57:54 +0100

    Write the code to draw the three-sided shape to ten-sided shape

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,18 +1,33 @@
+import random
 from turtle import Turtle, Screen
 
 
-def draw_dashed_line(length, dash_length=10, gap_length=10):
-    tim = Turtle()
-    tim.color("red")
-    tim.pensize(2)
-    for _ in range(length // (dash_length + gap_length)):
-        tim.forward(dash_length)
-        tim.penup()
-        tim.forward(gap_length)
-        tim.pendown()
+def random_color():
+    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+
+def draw_shape(turtle, sides, side_length=100):
+    turtle.color(random_color())
+    angle = 360 / sides
+    for _ in range(sides):
+        turtle.forward(side_length)
+        turtle.right(angle)
+
 
 
 if __name__ == "__main__":
     screen = Screen()
-    draw_dashed_line(200)
+    screen.colormode(255)
+
+    tim = Turtle()
+    tim.pensize(3)
+    tim.speed("fastest")
+
+    for sides in range(3, 11):
+        tim.penup()
+        tim.home()
+        tim.setheading(0)
+        tim.pendown()
+        draw_shape(tim, sides, 100)
+
     screen.exitonclick()
