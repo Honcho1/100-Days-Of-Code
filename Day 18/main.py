c8cfa669d159dbea44959f64567a08ddce86aa63
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2026-05-26 21:00:45 +0100

    Write the code to draw a dashed line

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,16 +1,18 @@
 from turtle import Turtle, Screen
 
 
-def draw_square(size):
+def draw_dashed_line(length, dash_length=10, gap_length=10):
     tim = Turtle()
-    tim.color("blue")
-    tim.pensize(3)
-    for _ in range(4):
-        tim.forward(size)
-        tim.right(90)
+    tim.color("red")
+    tim.pensize(2)
+    for _ in range(length // (dash_length + gap_length)):
+        tim.forward(dash_length)
+        tim.penup()
+        tim.forward(gap_length)
+        tim.pendown()
 
 
 if __name__ == "__main__":
     screen = Screen()
-    draw_square(100)
+    draw_dashed_line(200)
     screen.exitonclick()
