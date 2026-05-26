4165fb05bbbf622571a6cde394b5ff0c315c20fe
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2026-05-26 17:46:32 +0100

    Write the code to draw a square

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,1 +1,16 @@
 from turtle import Turtle, Screen
+
+
+def draw_square(size):
+    tim = Turtle()
+    tim.color("blue")
+    tim.pensize(3)
+    for _ in range(4):
+        tim.forward(size)
+        tim.right(90)
+
+
+if __name__ == "__main__":
+    screen = Screen()
+    draw_square(100)
+    screen.exitonclick()
